e530067597360143dcd0b334780c8d162521b21a
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-30 11:30:10 +0300

    написаны модели рассылки и сообщения

--- config/settings.py
+++ config/settings.py
@@ -65,15 +65,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": env.db()
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=30, verbose_name='имя')
+    last_name = models.CharField(max_length=30, verbose_name='фамилия')
+    email = models.EmailField(verbose_name='email')
+    note = models.CharField(max_length=300, verbose_name='комментарий')
+
+
+class Periods(models.TextChoices):
+    name = models.CharField(max_length=50, verbose_name='название')
+    pattern = models.CharField(max_length=20, verbose_name='cron-шаблон')
+
+
+class Message(models.Model):
+    body = models.TextField(verbose_name='содержимое')
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+
+
+class Mailing(models.Model):
+    STATUS_CREATED = 'created'
+    STATUS_STARTED = 'started'
+    STATUS_FINISHED = 'finished'
+
+    STATUSES = (
+        (STATUS_CREATED, 'Создана'),
+        (STATUS_STARTED, 'Запущена'),
+        (STATUS_FINISHED, 'Завершена'),
+    )
+
+    status = models.CharField(max_length=20, choices=STATUSES, verbose_name='статус', default=STATUS_CREATED)
+    period = models.ForeignKey(Periods, on_delete=models.CASCADE, verbose_name='периодичность')
+
+    recipients = models.ManyToManyField(Client, verbose_name='получатели')
+    start_time = models.DateTimeField(verbose_name='время начала')
+    end_time = models.DateTimeField(verbose_name='время окончания')
+    content = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='Содержание')
