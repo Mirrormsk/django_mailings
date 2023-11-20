3232ca96658fa09e8901739cb98dea581a5b412c
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-20 12:41:47 +0300

    реализован функционал верификации email

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .apps import UsersConfig
 from .views import (
     LoginView,
-    LogoutView, RegisterView,
+    LogoutView, RegisterView, activate_user,
     # RegisterView,
     # UserUpdateView,
     # activate_user,
@@ -18,9 +18,9 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("profile/<uuid:uid>/<token>/", activate_user, name="activate_user"),
     # path("profile/", UserUpdateView.as_view(), name="profile"),
     # # path("profile/generatepassword/", generate_new_password, name="generate_new_password"),
-    # path("profile/<uuid:uid>/verify/", activate_user, name="email_verify"),
     # path("activation_successful/", ActivationSuccessfulView.as_view(), name="activation_successful"),
     # path("profile/password-reset/", PasswordResetView.as_view(), name="password_reset"),
     # # path('reset-password/confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import get_user_model
 
+from users import texts
 from users.forms import UserRegisterForm
 
 
@@ -18,9 +25,51 @@ class LogoutView(BaseLogoutView):
 
 
 class RegisterView(CreateView):
-
     model = get_user_model()
     form_class = UserRegisterForm
     success_url = reverse_lazy("users:login")
     template_name = 'users/register.html'
 
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        current_site = get_current_site(self.request)
+        mail_subject = texts.register_mail_subject.format(current_site)
+        token = default_token_generator.make_token(user)
+        to_email = form.cleaned_data.get('email')
+
+        message = render_to_string('users/email_templates/confirm_email_message.html',
+                                   {
+                                       'user': user,
+                                       'current_site': current_site,
+                                       'domain': current_site.domain,
+                                       'uid': user.uid,
+                                       'token': token,
+                                   })
+
+        activation_email = EmailMessage(
+            subject=mail_subject,
+            body=message,
+            to=[to_email]
+        )
+
+        activation_email.content_subtype = "html"
+        activation_email.send()
+        return HttpResponse('Ссылка для подтверждения отправлена на ваш email.')
+
+
+def activate_user(request, uid, token):
+    UserModel: AbstractUser = get_user_model()
+    try:
+        user = UserModel.objects.get(uid=uid)
+    except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('Спасибо за регистрацию! Теперь вы можете войти в свой аккаунт')
+
+    return HttpResponse('Неверная ссылка активации!')
