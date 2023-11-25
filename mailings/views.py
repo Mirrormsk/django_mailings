f68b8e42091c0c0dd7860a75fa6379ca3adc1dab
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-25 14:25:11 +0300

    при создании рассылки юзер видит только свою аудиторию

--- mailings/views.py
+++ mailings/views.py
@@ -5,19 +5,27 @@ from django.forms import SplitDateTimeField
 from django.forms.widgets import SplitDateTimeWidget
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    TemplateView,
+)
 
 from blog.models import Article
 from .forms import AudienceForm
 from .models import Mailing, Client, Periods, MailingLog, Audience
 
-DATETIME_WIDGET = SplitDateTimeWidget(date_attrs={'type': 'date', 'class': 'my-2'}, time_attrs={'type': 'time'})
+DATETIME_WIDGET = SplitDateTimeWidget(
+    date_attrs={"type": "date", "class": "my-2"}, time_attrs={"type": "time"}
+)
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = 'mailings/index.html'
+    template_name = "mailings/index.html"
     extra_context = {
-        'nbar': 'home',
+        "nbar": "home",
     }
 
     def get_context_data(self, **kwargs):
@@ -26,18 +34,20 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
         all_mailings = user.mailing_set
         user_clients = user.client_set
-        random_articles = random.sample(list(Article.objects.filter(is_published=True)), 2)
+        random_articles = random.sample(
+            list(Article.objects.filter(is_published=True)), 2
+        )
 
         total_mailings = all_mailings.count()
-        started_mailings = all_mailings.filter(status='started').count()
-        waiting_for_start = all_mailings.filter(status='created').count()
+        started_mailings = all_mailings.filter(status="started").count()
+        waiting_for_start = all_mailings.filter(status="created").count()
         total_clients = user_clients.count()
 
-        context_data['total_mailings'] = total_mailings
-        context_data['started_mailings'] = started_mailings
-        context_data['waiting_for_start'] = waiting_for_start
-        context_data['total_clients'] = total_clients
-        context_data['random_articles'] = random_articles
+        context_data["total_mailings"] = total_mailings
+        context_data["started_mailings"] = started_mailings
+        context_data["waiting_for_start"] = waiting_for_start
+        context_data["total_clients"] = total_clients
+        context_data["random_articles"] = random_articles
 
         return context_data
 
@@ -46,15 +56,15 @@ class MailingListView(LoginRequiredMixin, ListView):
     model = Mailing
 
     extra_context = {
-        'title': 'Рассылки',
-        'nbar': 'mailings',
+        "title": "Рассылки",
+        "nbar": "mailings",
     }
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset()
         user = self.request.user
 
-        if not (user.has_perm('mailings.view_all_mailings') or user.is_superuser):
+        if not (user.has_perm("mailings.view_all_mailings") or user.is_superuser):
             queryset = queryset.filter(creator=user)
 
         return queryset
@@ -62,22 +72,37 @@ class MailingListView(LoginRequiredMixin, ListView):
 
 class MailingDeleteView(DeleteView):
     model = Mailing
-    success_url = reverse_lazy('mailings:mailings_list')
+    success_url = reverse_lazy("mailings:mailings_list")
 
 
 class MailingCreateView(CreateView):
     model = Mailing
-    fields = ('name', 'period', 'audience', 'start_time', 'end_time', 'message_title', 'message_body')
-    success_url = reverse_lazy('mailings:mailings_list')
+    fields = (
+        "name",
+        "period",
+        "audience",
+        "start_time",
+        "end_time",
+        "message_title",
+        "message_body",
+    )
+    success_url = reverse_lazy("mailings:mailings_list")
     extra_context = {
-        'title': 'Создать рассылку',
-        'nbar': 'mailings',
+        "title": "Создать рассылку",
+        "nbar": "mailings",
     }
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields['start_time'] = SplitDateTimeField(widget=DATETIME_WIDGET, label='Время начала')
-        form.fields['end_time'] = SplitDateTimeField(widget=DATETIME_WIDGET, label='Время завершения')
+        form.fields["start_time"] = SplitDateTimeField(
+            widget=DATETIME_WIDGET, label="Время начала"
+        )
+        form.fields["end_time"] = SplitDateTimeField(
+            widget=DATETIME_WIDGET, label="Время завершения"
+        )
+        form.fields["audience"].queryset = Audience.objects.filter(
+            creator=self.request.user
+        )
         return form
 
     def form_valid(self, form):
@@ -91,19 +116,33 @@ class MailingCreateView(CreateView):
 
 class MailingUpdateView(UserPassesTestMixin, UpdateView):
     model = Mailing
-    fields = ('name', 'period', 'audience', 'start_time', 'end_time', 'message_title', 'message_body')
-    success_url = reverse_lazy('mailings:mailings_list')
+    fields = (
+        "name",
+        "period",
+        "audience",
+        "start_time",
+        "end_time",
+        "message_title",
+        "message_body",
+    )
+    success_url = reverse_lazy("mailings:mailings_list")
 
     extra_context = {
-        'title': 'Редактировать рассылку',
-        'nbar': 'mailings',
+        "title": "Редактировать рассылку",
+        "nbar": "mailings",
     }
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields['start_time'] = SplitDateTimeField(widget=DATETIME_WIDGET, label='Время начала')
-        form.fields['end_time'] = SplitDateTimeField(widget=DATETIME_WIDGET, label='Время начала')
-        form.fields['audience'].queryset = Audience.objects.filter(creator=self.request.user)
+        form.fields["start_time"] = SplitDateTimeField(
+            widget=DATETIME_WIDGET, label="Время начала"
+        )
+        form.fields["end_time"] = SplitDateTimeField(
+            widget=DATETIME_WIDGET, label="Время начала"
+        )
+        form.fields["audience"].queryset = Audience.objects.filter(
+            creator=self.request.user
+        )
         return form
 
     def test_func(self):
@@ -113,13 +152,10 @@ class MailingUpdateView(UserPassesTestMixin, UpdateView):
 
 class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
-    fields = ('first_name', 'last_name', 'email', 'note')
-    success_url = reverse_lazy('mailings:client_list')
+    fields = ("first_name", "last_name", "email", "note")
+    success_url = reverse_lazy("mailings:client_list")
 
-    extra_context = {
-        'title': 'Добавить получателя',
-        'nbar': 'clients'
-    }
+    extra_context = {"title": "Добавить получателя", "nbar": "clients"}
 
     def form_valid(self, form):
         user = self.request.user
@@ -132,16 +168,13 @@ class ClientCreateView(LoginRequiredMixin, CreateView):
 class ClientListView(ListView):
     model = Client
 
-    extra_context = {
-        'title': 'Клиенты',
-        'nbar': 'clients'
-    }
+    extra_context = {"title": "Клиенты", "nbar": "clients"}
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset()
         user = self.request.user
 
-        if not (user.has_perm('mailings.view_all_mailings') or user.is_superuser):
+        if not (user.has_perm("mailings.view_all_mailings") or user.is_superuser):
             queryset = queryset.filter(creator=user)
 
         return queryset
@@ -149,12 +182,12 @@ class ClientListView(ListView):
 
 class PeriodsCreateView(CreateView):
     model = Periods
-    fields = ('name', 'duration')
-    success_url = reverse_lazy('mailings:periods_list')
+    fields = ("name", "duration")
+    success_url = reverse_lazy("mailings:periods_list")
 
     extra_context = {
-        'title': 'Добавить период',
-        'nbar': 'periods',
+        "title": "Добавить период",
+        "nbar": "periods",
     }
 
 
@@ -162,8 +195,8 @@ class PeriodsListView(ListView):
     model = Periods
 
     extra_context = {
-        'title': 'Периоды',
-        'nbar': 'periods',
+        "title": "Периоды",
+        "nbar": "periods",
     }
 
 
@@ -173,20 +206,22 @@ class MailingLogListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context_data = super().get_context_data(**kwargs)
 
-        context_data['mailings'] = Mailing.objects.filter(creator=self.request.user)
-        context_data['title'] = 'Логи'
-        context_data['nbar'] = 'logs'
-        context_data['selected_mailing_pk'] = int(self.request.POST.get('mailing', 0))
+        context_data["mailings"] = Mailing.objects.filter(creator=self.request.user)
+        context_data["title"] = "Логи"
+        context_data["nbar"] = "logs"
+        context_data["selected_mailing_pk"] = int(self.request.POST.get("mailing", 0))
 
         return context_data
 
     def get_queryset(self):
-        mailing_pk = self.request.POST.get('mailing', 0)
+        mailing_pk = self.request.POST.get("mailing", 0)
 
-        mailing_pk_list = list(Mailing.objects.values_list('pk', flat=True))
+        mailing_pk_list = list(Mailing.objects.values_list("pk", flat=True))
 
         if int(mailing_pk) in mailing_pk_list:
-            return self.model.objects.filter(mailing_id=mailing_pk, mailing__creator=self.request.user)
+            return self.model.objects.filter(
+                mailing_id=mailing_pk, mailing__creator=self.request.user
+            )
         return self.model.objects.filter(mailing__creator=self.request.user)
 
     def post(self, request):
@@ -197,15 +232,15 @@ class AudienceListView(ListView):
     model = Audience
 
     extra_context = {
-        'title': "Аудитории",
-        'nbar': 'audiences',
+        "title": "Аудитории",
+        "nbar": "audiences",
     }
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset()
         user = self.request.user
 
-        if not (user.has_perm('mailings.view_all_mailings') or user.is_superuser):
+        if not (user.has_perm("mailings.view_all_mailings") or user.is_superuser):
             queryset = queryset.filter(creator=user)
 
         return queryset
@@ -214,25 +249,30 @@ class AudienceListView(ListView):
 class AudienceCreateView(LoginRequiredMixin, CreateView):
     model = Audience
     form_class = AudienceForm
+    success_url = reverse_lazy("mailings:audiences_list")
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
+        kwargs["user"] = self.request.user
         return kwargs
 
-
-
+    def form_valid(self, form):
+        user = self.request.user
+        audience = form.save(commit=False)
+        audience.creator = user
+        audience.save()
+        return super().form_valid(form)
 
 
 def stop_mailing(request, pk):
     mailing = Mailing.objects.get(pk=pk)
     mailing.status = Mailing.STATUS_FINISHED
     mailing.save()
-    return redirect(reverse_lazy('mailings:mailings_list'))
+    return redirect(reverse_lazy("mailings:mailings_list"))
 
 
 def start_mailing(request, pk):
     mailing = Mailing.objects.get(pk=pk)
     mailing.status = Mailing.STATUS_CREATED
     mailing.save()
-    return redirect(reverse_lazy('mailings:mailings_list'))
+    return redirect(reverse_lazy("mailings:mailings_list"))
