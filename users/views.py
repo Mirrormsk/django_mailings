128477aad7c7b6bf4e37c7305d7a4da1be296e2d
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-26 11:18:06 +0300

    Login required настроен в контроллерах приложения Users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LoginView as BaseLoginView
@@ -18,9 +18,7 @@ from users.forms import UserRegisterForm
 
 class LoginView(BaseLoginView):
 
-    extra_context = {
-        'title': 'Сервис рассылок - Вход'
-    }
+    extra_context = {"title": "Сервис рассылок - Вход"}
 
 
 class LogoutView(BaseLogoutView):
@@ -31,7 +29,7 @@ class RegisterView(CreateView):
     model = get_user_model()
     form_class = UserRegisterForm
     success_url = reverse_lazy("users:login")
-    template_name = 'users/register.html'
+    template_name = "users/register.html"
 
     def form_valid(self, form):
         user = form.save(commit=False)
@@ -41,36 +39,36 @@ class RegisterView(CreateView):
         current_site = get_current_site(self.request)
         mail_subject = texts.register_mail_subject.format(current_site)
         token = default_token_generator.make_token(user)
-        to_email = form.cleaned_data.get('email')
+        to_email = form.cleaned_data.get("email")
 
-        message = render_to_string('users/email_templates/confirm_email_message.html',
-                                   {
-                                       'user': user,
-                                       'current_site': current_site,
-                                       'domain': current_site.domain,
-                                       'uid': user.uid,
-                                       'token': token,
-                                   })
+        message = render_to_string(
+            "users/email_templates/confirm_email_message.html",
+            {
+                "user": user,
+                "current_site": current_site,
+                "domain": current_site.domain,
+                "uid": user.uid,
+                "token": token,
+            },
+        )
 
         activation_email = EmailMessage(
-            subject=mail_subject,
-            body=message,
-            to=[to_email]
+            subject=mail_subject, body=message, to=[to_email]
         )
 
         activation_email.content_subtype = "html"
         activation_email.send()
-        return HttpResponse('Ссылка для подтверждения отправлена на ваш email.')
+        return HttpResponse("Ссылка для подтверждения отправлена на ваш email.")
 
 
-class UserListView(PermissionRequiredMixin, ListView):
+class UserListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = get_user_model()
 
-    permission_required = 'users.view_user'
+    permission_required = "users.view_user"
 
     extra_context = {
-        'title': 'Пользователи',
-        'nbar': 'users',
+        "title": "Пользователи",
+        "nbar": "users",
     }
 
 
@@ -84,12 +82,14 @@ def activate_user(request, uid, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return HttpResponse('Спасибо за регистрацию! Теперь вы можете войти в свой аккаунт')
+        return HttpResponse(
+            "Спасибо за регистрацию! Теперь вы можете войти в свой аккаунт"
+        )
 
-    return HttpResponse('Неверная ссылка активации!')
+    return HttpResponse("Неверная ссылка активации!")
 
 
-@permission_required('users.block_user',  raise_exception=True)
+@permission_required("users.block_user", raise_exception=True)
 def deactivate_user(request, uid):
     UserModel = get_user_model()
     try:
@@ -100,6 +100,6 @@ def deactivate_user(request, uid):
     if user is not None:
         user.is_active = False
         user.save()
-        return HttpResponse(f'Пользователь с почтой {user.email} заблокирован.')
+        return HttpResponse(f"Пользователь с почтой {user.email} заблокирован.")
 
-    return HttpResponse('Пользователь не найден')
+    return HttpResponse("Пользователь не найден")
