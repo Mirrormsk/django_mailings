c2bac089c05e4e438d9f37e386866af8b607ddfc
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-27 17:37:18 +0300

    страница детального просмотра статьи кеширована

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .apps import BlogConfig
 from .views import ArticleListView, ArticleCreateView, ArticleUpdateView, ArticleDetailView
@@ -7,7 +8,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name='blog_list'),
-    path("article/<int:pk>", ArticleDetailView.as_view(), name='article_detail'),
+    path("article/<int:pk>", cache_page(60)(ArticleDetailView.as_view()), name='article_detail'),
     path("create/", ArticleCreateView.as_view(), name='article_create'),
     path("edit/<int:pk>", ArticleUpdateView.as_view(), name='article_edit'),
 ]
